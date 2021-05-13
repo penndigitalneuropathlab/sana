942b87720e131d483c4c951323efc93ab21d15f3
Noah Capp <noahmcapp@gmail.com>
2021-05-13 16:06:57 -0400

    started re-write off GM segmentation script

--- scripts/sana_gm_segmentation.py
+++ scripts/sana_gm_segmentation.py
@@ -9,6 +9,7 @@ import argparse
 from scipy import signal
 from matplotlib import pyplot as plt
 import seaborn as sns; sns.set()
+from PIL import Image
 
 import sana_io
 from sana_frame import Frame
@@ -16,6 +17,7 @@ from sana_geo import Point, Polygon, ray_tracing
 from sana_color_deconvolution import StainSeparator
 from sana_tiler import Tiler
 from sana_loader import Loader
+from sana_thresholds import knn
 
 USAGE = os.path.expanduser(
     '~/neuropath/src/sana/scripts/usage/sana_roi_analysis.usage')
@@ -38,188 +40,261 @@ def main(argv):
 
     # loop through the slides
     for slide_i, slide_f in enumerate(slides):
-        print("--> Processing: %s (%d/%d)" % (os.path.basename(slide_f), slide_i+1, len(slides)))
-        loader = Loader(slide_f)
-        converter = loader.converter
-        loader.set_lvl(args.lvl)
 
         # get the annotation file
         anno_f = sana_io.slide_to_anno(slide_f, adir=args.adir, rdir=args.rdir)
 
-        # TODO: this threshold is not consistent!!
+        # define the output file location
+        out_f = sana_io.get_ofname(anno_f, odir=args.odir)
+
+        if 'NeuN' in slide_f:
+            target = 'DAB'
+        else:
+            target = 'HEM'
+
+        print("--> Processing: %s (%d/%d)" % (os.path.basename(slide_f), slide_i+1, len(slides)))
+        try:
+            loader = Loader(slide_f)
+        except:
+            print("Slide not found:", slide_f)
+            continue
+        converter = loader.converter
+        loader.set_lvl(args.lvl)
+
+        # load the ROIs to process
+        rois = sana_io.read_annotations(anno_f, args.roi_class)
+        if rois is None or len(rois[0]) == 0:
+            print(os.path.exists(anno_f))
+            print("Skipping Annotations: %s" % anno_f)
+            continue
+
+        segs = []
+        for roi_i, roi in enumerate(rois[0]):
+            print(slide_i, '/', len(slides), '-->', roi_i, '/', len(rois[0]), flush=True)
+
+            frame_density, metrics = get_density(args, loader, out_f, roi_i, roi)
+            if metrics is None:
+                continue
+            angle, loc, crop_loc, ds = metrics
+
+            # apply KNN to get the 3 most prominent values
+            # NOTE: in DAB this will be Slide/WM, low GM, and high GM
+            # NOTE: in HEM this will be Slide, GM, and WM
+            seg, thresholds = knn(frame_density, 3)
+            d_seg = seg
+            seg = Frame(seg)
+            if target == 'DAB':
+                seg.threshold(thresholds[0], x=0, y=1)
+                seg = seg.img[:, :, 0]
+            else:
+                seg.threshold(thresholds[1], x=1, y=0)
+                seg = seg.img[:, :, 0]
+
+            # define the min thickness of layer 0 and layer 6
+            # TODO: needs to be defined in microns, won't work with different
+            #        step values
+            N0, N6 = 3, 5
+            l0, l6 = [], []
+
+            # loop from bottom to top in each column, find the first instance
+            # of positive thresholded data
+            for i in range(seg.shape[1]):
+                x = seg[:, i]
+                found = False
+                for j in range(x.shape[0]-1, -1, -1):
+                    if all(x[j-N6:j] == 1):
+                        l6.append(j)
+                        found = True
+                        break
+                if not found:
+                    l6.append(x.shape[0]-1)
+            l6 = np.array(signal.medfilt(l6, 7))
+            l6 = np.rint(l6).astype(int)
+
+            # remove the WM using the detected boundary
+            for i in range(frame_density.shape[1]):
+                frame_density[l6[i]:, i] = 0
+
+            # if we're using HEM stain, need to re-run knn
+            # to get slide, low GM, and high GM
+            if target == 'HEM':
+                seg, thresholds = knn(frame_density, 3)
+                seg = Frame(seg)
+                seg.threshold(thresholds[0], x=0, y=1)
+                seg = seg.img[:, :, 0]
+
+            # loop from top to bottom of each column, find the first
+            # instance of positive thresholded data
+            for i in range(seg.shape[1]):
+                x = seg[:, i]
+                found = False
+                for j in range(0, x.shape[0], 1):
+                    if all(x[j:j+N0] == 1):
+                        l0.append(j)
+                        found = True
+                        break
+                if not found:
+                    l0.append(0)
+            l0 = np.array(signal.medfilt(l0, 7))
+            l0 = np.rint(l0).astype(int)
+            d_seg = seg
+
+            # build the gm segmentation by combining layers 0 and 6
+            i = np.arange(len(l0))
+            x = np.concatenate([i[::-1], i])
+            y = np.concatenate([l0[::-1], l6])
+            seg = Polygon(x, y, False, loader.lvl).astype(float)
+
+            # revert to original orientation, location, and resolution
+            seg[:, 0] *= ds[0]
+            seg[:, 1] *= ds[1]
+
+            frame_thresh = Image.open(out_f.replace('.json', '_%d_THRESH.png') % roi_i)
+
+            seg.translate(-crop_loc)
+            roi.translate(loc)
+            seg = seg.rotate(roi.centroid()[0], -angle)
+            seg.translate(-loc)
+            loader.converter.rescale(seg, 0)
+            print(loc, crop_loc, roi.centroid(), seg.centroid())
+
+            # connect the last point to the first point
+            seg = seg.connect()
+            segs.append(seg)
+        #
+        # end of gm_rois loop
+
+        sana_io.write_annotations(out_f, segs, class_name=args.seg_class)
+    #
+    # end of slides loop
+#
+# end of main
+
+def get_density(args, loader, out_f, roi_i, roi):
+    if os.path.exists(out_f.replace('.json', '_%d_DENSITY.npy') % roi_i) and \
+    os.path.exists(out_f.replace('.json', '_%d_DAT.csv') % roi_i):
+        frame_density = np.load(out_f.replace('.json', '_%d_DENSITY.npy') % roi_i)
+        fp = open(out_f.replace('.json', '_%d_DAT.csv') % roi_i)
+        angle, l0, l1, c0, c1, ds0, ds1 = fp.read().split('\n')[0].split(',')
+        loc = Point(float(l0), float(l1), False, 0)
+        crop_loc = Point(float(c0), float(c1), False, 0)
+        ds = Point(float(ds0), float(ds1), False, 0)
+        metrics = [float(angle), loc, crop_loc, ds]
+    else:
         # pre-calculate the tissue threshold
         print("----> Pre-Calculating the Tissue/Slide Threshold")
         thumbnail = loader.thumbnail.copy()
         tissue_threshold = thumbnail.get_tissue_threshold(blur=5)
 
-        # loop through the GM ROIs and generate the segmentations
-        gm_segs = []
-        gm_seg_metrics = []
-        gm_rois, _, gm_names = sana_io.read_annotations(
-            anno_f, args.roi_class)
-        for gm_roi_i, gm_roi in enumerate(gm_rois):
-            print("\n----> Segmenting GM ROI (%d/%d)" % \
-                  (gm_roi_i+1, len(gm_rois)))
-            t0 = time.time()
-            t1 = time.time()
+        # define the bounding box of the ROI
+        c, r = roi.centroid()
+        loc = Point(c[0]-r, c[1]-r, False, 0)
+        size = Point(2*r, 2*r, False, 0)
+        roi.translate(loc)
+        t1 = time.time()
 
-            # define the bounding box of the ROI
-            c, r = gm_roi.centroid()
-            roi_loc = Point(c[0]-r, c[1]-r, False, 0)
-            roi_size = Point(2*r, 2*r, False, 0)
+        # calculate the angular rotation of the tissue/slide boundary
+        print("------> Detecting Tissue Rotation: ", end="")
+        try:
+            frame_thumb = loader.load_frame(loc, size, lvl=loader.lc-1)
+        except:
+            print("Load Failed --", slide_f, '--', roi_i)
+        converter = loader.converter
+        converter.rescale(roi, loader.lc-1)
+        angle = frame_thumb.tissue_angle(tissue_threshold, roi, 1e5, 1e5)
+        converter.rescale(roi, loader.lvl)
+        converter.rescale(loc, loader.lvl)
+        converter.rescale(size, loader.lvl)
+        print("%.3f sec" % (time.time() - t1))
+        t1 = time.time()
+        if angle is None:
+            return None, None
 
-            # load the thumbnail resolution roi, get the direction of the tissue
-            print("------> Detecting Tissue Rotation: ", end="")
-            gm_roi.translate(roi_loc)
-            roi_thumb = loader.load_frame(roi_loc, roi_size, lvl=loader.lc-1)
-            angle = roi_thumb.tissue_angle(tissue_threshold, gm_roi, 1e5, 1e5)
-            converter.rescale(gm_roi, loader.lvl)
-            converter.rescale(roi_loc, loader.lvl)
-            converter.rescale(roi_size, loader.lvl)
-            print("%.3f sec" % (time.time() - t1))
-            t1 = time.time()
+        # load the frame based on the radius of the ROI
+        print("------> Loading Frame: ", end="", flush=True)
+        frame = loader.load_frame(loc, size)
+        print("%.3f sec" % (time.time() - t1))
+        t1 = time.time()
 
-            gm_roi_rot = gm_roi.rotate(gm_roi.centroid()[0], angle)
-            print("------> Loading Frame: ", end="", flush=True)
-            frame = loader.load_frame(roi_loc, roi_size)
-            print("%.3f sec" % (time.time() - t1))
-            t1 = time.time()
+        # rotate the frame around the centroid of the ROI
+        print("------> Rotating Frame: ", end="", flush=True)
+        rot_center = roi.centroid()[0]
+        roi = roi.rotate(rot_center, angle)
+        frame = frame.rotate(angle)
+        crop_loc, crop_size = roi.bounding_box()
+        frame = frame.crop(crop_loc, crop_size)
+        roi.translate(crop_loc)
+        print("%.3f sec" % (time.time() - t1))
+        t1 = time.time()
 
-            print("------> Rotating Frame: ", end="", flush=True)
-            frame_rot = frame.rotate(angle)
-            crop_loc, crop_size = gm_roi_rot.bounding_box()
-            frame_rot = frame_rot.crop(crop_loc, crop_size)
-            gm_roi_rot.translate(crop_loc)
-            print("%.3f sec" % (time.time() - t1))
-            t1 = time.time()
+        # generate the tissue mask from the rotated ROI
+        print("------> Detecting Tissue in ROI: ", end="", flush=True)
+        tissue_mask = frame.copy()
+        tissue_mask.detect_tissue(tissue_threshold, 1e5, 1e5)
+        print("%.3f sec" % (time.time() - t1))
+        t1 = time.time()
 
-            # generate the tissue mask from the rotated ROI
-            print("------> Detecting Tissue in ROI: ", end="", flush=True)
-            tissue_mask = frame_rot.copy()
-            tissue_mask.detect_tissue(tissue_threshold, 1e5, 1e5)
-            print("%.3f sec" % (time.time() - t1))
-            t1 = time.time()
+        # separate the stain from the ROI using color deconvolution
+        print("------> Stain Separating: ", end="", flush=True)
+        if 'NeuN' in out_f:
+            target = 'DAB'
+        else:
+            target = 'HEM'
+        separator = StainSeparator(args.stain, target, ret_od=True)
+        frame_stain = separator.run(frame.img)[0]
+        frame_stain = Frame(frame_stain, loader.lvl, converter)
+        frame_stain.save(out_f.replace('.json', '_%d_STAIN.png') % roi_i)
+        print("%.3f sec" % (time.time() - t1))
+        t1 = time.time()
 
-            print("------> Masking out Background: ", end="", flush=True)
-            tissue_mask.to_mask(x=0, y=255)
-            print("%.3f sec" % (time.time() - t1))
-            t1 = time.time()
+        print("------> Stain Thresholding: ", end="", flush=True)
+        stain_threshold = frame_stain.copy().get_stain_threshold(tissue_mask)
+        frame_thresh = frame_stain.copy()
+        frame_thresh.threshold(stain_threshold, x=0, y=1)
+        frame_thresh.save(out_f.replace('.json', '_%d_THRESH.png') % roi_i)
+        print("%.3f sec" % (time.time() - t1))
+        t1 = time.time()
 
-            # separate the stain from the ROI using color deconvolution
-            print("------> Stain Separating: ", end="", flush=True)
-            separator = StainSeparator(args.stain, args.target, ret_od=False)
-            frame_stain = separator.run(frame_rot.img)[0]
-            frame_stain = Frame(frame_stain, loader.lvl, converter)
-            frame_stain.to_gray()
-            print("%.3f sec" % (time.time() - t1))
-            t1 = time.time()
+        print("------> Calculating Tile Features", end="", flush=True)
 
-            print("------> Stain Thresholding: ", end="", flush=True)
-            stain_threshold = frame_stain.copy().get_stain_threshold(tissue_mask)
-            frame_thresh = frame_stain.copy()
-            frame_thresh.threshold(stain_threshold, x=1, y=0)
-            print("%.3f sec" % (time.time() - t1))
-            t1 = time.time()
+        # load the tiles
+        tsize = Point(args.tsize[0], args.tsize[1], True)
+        tstep = Point(args.tstep[0], args.tstep[1], True)
+        tiler = Tiler(loader.lvl, loader.converter, tsize, tstep)
+        tiler.set_frame(frame_thresh, pad=True)
+        tiles = tiler.load_tiles()
+        frame_density = np.zeros((tiles.shape[0], tiles.shape[1]), dtype=float)
+        frame_size = np.zeros((tiles.shape[0], tiles.shape[1]), dtype=float)
 
-            print("------> Calculating Stain Density: ", end="", flush=True)
+        metrics = [angle, loc, crop_loc, tiler.ds]
+        fp = open(out_f.replace('.json', '_%d_DAT.csv' % roi_i), 'w')
+        fp.write('%f,%f,%f,%f,%f,%f,%f\n' % (angle, loc[0], loc[1], crop_loc[0], crop_loc[1], tiler.ds[0], tiler.ds[1]))
+        fp.close()
 
-            # load the tiles
-            tsize = Point(args.tsize[0], args.tsize[1], True)
-            tstep = Point(args.tstep[0], args.tstep[1], True)
-            tiler = Tiler(loader.lvl, loader.converter, tsize, tstep)
-            tiles = tiler.load_tiles(frame_thresh, pad=True)
-
-            # calculate the density of the stain in the frame
-            frame_density = Frame(
-                np.mean(tiles, axis=(2,3)), loader.lvl, loader.converter)
-            print("%.3f sec" % (time.time() - t1))
-            t1 = time.time()
-
-            # detect the layer 6 boundary from the density frame
-            # NOTE: this is scaled up from the tile dimension
-            layer_6_threshold = 0.2
-            x_6, y_6 = frame_density.copy().detect_layer_6(layer_6_threshold)
-            x_6 = np.rint(x_6 * tiler.ds[0]).astype(np.int)
-            y_6 = np.rint(y_6 * tiler.ds[1]).astype(np.int)
-
-            # detect the layer 0 boundary from the tissue mask
-            x_0, y_0 = tissue_mask.copy().detect_layer_0(xvals=x_6)
-            x_0 = np.rint(x_0).astype(np.int)
-            y_0 = np.rint(y_0).astype(np.int)
-
-            print(len(x_6), len(x_0))
-            # smooth the detections
-            y_0 = signal.medfilt(y_0, 31)
-            y_6 = signal.medfilt(y_6, 31)
-
-            # filter the detections by the given roi, also filter so that
-            #   layer 0 or layer 6 don't extend past each other
-            v_0 = np.array((x_0, y_0)).T
-            v_6 = np.array((x_6, y_6)).T
-            v_0 = [v for v in v_0 if ray_tracing(v[0], v[1], gm_roi_rot)]
-            v_6 = [v for v in v_6 if ray_tracing(v[0], v[1], gm_roi_rot)]
-            left, right = max(v_0[0][0], v_6[0][0]), min(v_0[-1][0], v_6[-1][0])
-            v_0 = np.array([v for v in v_0 if v[0] >= left and v[0] <= right])
-            v_6 = np.array([v for v in v_6 if v[0] >= left and v[0] <= right])
-
-            # build the gm segmentation by combining layers 0 and 6
-            x = np.concatenate([v_0[::-1][:, 0], v_6[:, 0]])
-            y = np.concatenate([v_0[::-1][:, 1], v_6[:, 1]])
-            gm_seg = Polygon(x, y, False, loader.lvl).astype(float)
-
-            # revert to original orientation, location, and resolution
-            gm_seg = gm_seg.rotate(gm_roi_rot.centroid()[0], -angle)
-            gm_seg.translate(-(crop_loc + roi_loc))
-            loader.converter.rescale(gm_seg, 0)
-
-            # connect the last point to the first point
-            gm_seg = gm_seg.connect()
-            gm_segs.append(gm_seg)
-
-            # gm_plot = copy(gm_seg)
-            # loader.converter.rescale(gm_plot, loader.lvl)
-            # gm_plot.translate(roi_loc)
-            # fig, axs = plt.subplots(2,2)
-            # axs = axs.ravel()
-            # axs[0].imshow(frame_stain.img)
-            # for i in range(gm_roi_rot.shape[0]-1):
-            #     axs[0].plot(gm_roi_rot[i:i+2, 0], gm_roi_rot[i:i+2, 1], color='black')
-            # axs[1].imshow(frame_thresh.img)
-            # axs[2].imshow(frame_density.img)
-            # axs[3].imshow(frame.img)
-            # for i in range(gm_plot.shape[0]-1):
-            #     axs[3].plot(gm_plot[i:i+2, 0], gm_plot[i:i+2, 1], color='black')
-            # plt.show()
-
-            # gm_seg, angle, metrics = sana_proc.segment_gm(
-            #     args, slide_f, gm_roi, tissue_threshold, count=gm_roi_i)
-            # gm_seg_metrics.append(metrics)
-        #
-        # end of gm_rois loop
-
-        sana_io.append_annotations(anno_f, gm_segs,
-                                   class_name=args.seg_class,
-                                   anno_names=gm_names)
-    #
-    # end of slides loop
-
-        # store information about the processing that occurred
-        # info.append(map(str, [slide_f, loader.mpp, loader.ds, loader.lvl, list(trans_loc0), list(trans_loc1), list(centroid), radius, angle, list(tds), tissue_threshold, stain_threshold, angle, list(args.tsize), list(args.tstep)]))
-        #
-        # end of annos loop
-    #
-    # end of slides loop
-
-    # # write the info related to the processing parameters
-    # info_of = sana_io.get_ofname(
-    #     'information.csv', odir=args.odir, rdir=args.rdir)
-    # fp = open(info_of, 'w')
-    # for x in info:
-    #     fp.write('\t'.join(x) + '\n')
-    # fp.close()
-
-#
-# end of main
+        for j in range(tiles.shape[1]):
+            for i in range(tiles.shape[0]):
+                s = ' -- %d/%d:' % (j*tiles.shape[0]+i,
+                    tiles.shape[0]*tiles.shape[1])
+                print(s + "\b"*len(s), flush=True, end="")
+                frame = Frame(tiles[i][j], 0, converter)
+                frame.detect_cells(min_body=15, max_body=1000)
+                count = len(frame.get_bodies())
+                areas = np.array([c.polygon.area() for c in frame.get_bodies()])
+                areas = areas[areas > 50]
+                if len(areas) < 5:
+                    area = 0
+                else:
+                    area = np.mean(areas)
+                frame_density[i][j] = count
+                frame_size[i][j] = area
+        np.save(out_f.replace('.json', '_%d_DENSITY.npy' % roi_i), frame_density)
+        np.save(out_f.replace('.json', '_%d_SIZE.npy' % roi_i), frame_size)
+        roi.translate(-crop_loc)
+        roi = roi.rotate(rot_center, -angle)
+        roi.translate(-loc)
+        print("%.3f sec" % (time.time() - t1), flush=True)
+        t1 = time.time()
+    return frame_density, metrics
 
 def cmdl_parser(argv):
     parser = argparse.ArgumentParser(usage=open(USAGE).read())
@@ -236,8 +311,6 @@ def cmdl_parser(argv):
                         help="tile step for analysis\n[default: 25 25]")
     parser.add_argument('-stain', type=str, default="H-DAB",
                         help="define the Stain type (H-DAB, HED)\n[default: H-DAB]")
-    parser.add_argument('-target', type=str, default="DAB",
-                        help="define the Stain target (HEM, DAB, EOS, RES, ALL) \n[default: DAB]")
     parser.add_argument('-adir', type=str, default="",
                         help="specify the location of annotation files\n[default: same as slide]")
     parser.add_argument('-odir', type=str, default="",
