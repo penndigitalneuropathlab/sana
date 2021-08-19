5b00f3c37bea8484e2ebafab0252bda51bb4658a
Noah Capp <noahmcapp@gmail.com>
2021-08-19 13:00:10 -0400

    added comments and rewrote some functions in Converter and Tiler

--- classes/sana_tiler.py
+++ classes/sana_tiler.py
@@ -1,34 +1,52 @@
 
+# system packages
 from copy import copy
+
+# installed packages
 import numpy as np
 
+# custom packages
 from sana_framer import Framer
+from sana_geo import round
 
+# custom Exceptions
+ERR = "---> %s <---"
+ERR_FRAMER = ERR % ("Tiler doesn't have a Framer, must provide fsize and fstep")
+ERR_FRAME = ERR % ("Frame must be a single channel image (m, n, 1)")
+class TilerException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+# creates a set of tiles from a Frame with a given size and step length
+# Frames can be set manually, or generated using a Framer that Tiler creates
+# the tiles are center-aligned on the frame, some Frame padding occurs
+#  -lvl: pixel resolution to use
+#  -converter: Converter to handle unit conversion
+#  -tsize: Point, defines the size of each tile
+#  -tstep: Point, defines the distance between each tile, tsize if not given
+#  -fsize: Point, defines the fsize for a Framer if needed
+#  -fstep: Point, defines the fstep for a Framer if needed
+#  -loader: Loader to load the frames for a Framer if needed
 class Tiler:
-    def __init__(self, lvl, converter, tsize, tstep=None,
+    def __init__(self, lvl, converter, size, step=None,
                  fsize=None, fstep=None, loader=None):
         self.lvl = lvl
         self.converter = converter
 
-        # define the tile size and step size
-        self.size = tsize
+        # set the tile size, convert to pixels, and round
+        self.size = size
+        self.converter.to_pixels(self.size, self.lvl)
+        self.size = self.converter.to_int(self.size)
+
+        # set the tile step, convert to pixels, and round
         if tstep is None:
             self.step = copy(self.size)
         else:
             self.step = tstep
+        self.converter.to_pixels(self.step, self.lvl)
+        self.step = self.converter.to_int(self.step)
 
-        # convert the dimensions to pixels and round
-        if self.size.is_micron:
-            self.converter.to_pixels(self.size, self.lvl)
-        self.converter.rescale(self.size, self.lvl)
-        if self.step.is_micron:
-            self.converter.to_pixels(self.step, self.lvl)
-        self.converter.rescale(self.step, self.lvl)
-
-        self.size = np.rint(self.size).astype(np.int)
-        self.step = np.rint(self.step).astype(np.int)
-
-        # define the amount to pad/shift the frames for center-alignment
+        # calculate the amount to pad and shift a frame for center alignment
         self.fpad = self.size - 1
         self.fshift = self.size // 2
 
@@ -36,32 +54,41 @@ class Tiler:
         if fsize is not None:
             self.framer = Framer(
                 loader, fsize, fstep, fpad=self.fpad, fshift=self.fshift)
+    #
+    # end of constructor
 
+    # loads a frame into memory using the Framer
     def load_frame(self, i, j):
+        if not hasattr(self, 'framer'):
+            raise TilerException(ERR_FRAMER)
         return self.framer.load(i, j)
+    #
+    # end of load_frame
 
+    # sets the current Frame to be used by the Tiler
+    # NOTE: if setting Frame manually, set pad=True for center-alignment
     def set_frame(self, frame, pad=False):
+        if frame.shape[2] != 1:
+            raise TilerException(ERR_FRAME)
+
+        # pad the frame if needed
         if pad:
             frame = frame.pad(self.fpad)
 
-        # set the frame and calculate the number of tiles in the frame
+        # store the frame
         self.frame = frame
+
+        # TODO: check if this is necessary? might mess with the stride_tricks
         self.frame.img = self.frame.img[:, :, 0]
+
+        # calculate the number of tiles in the frame and the downsample factor
         self.n = ((self.frame.size() - self.size) // self.step) + 1
         self.ds = (self.frame.size() - self.fpad) / self.n
+    #
+    # end of set_frame
 
-    def get_tile_bounds(self, frame=None, pad=False):
-        if not frame is None:
-            self.set_frame(frame, pad)
-
-        for i in range(self.n[0]):
-            for j in range(self.n[1]):
-                x0 = i * self.step[0]
-                y0 = j * self.step[1]
-                x1 = x0 + self.size[0]
-                y1 = y0 + self.size[1]
-                yield i, j, x0, y0, x1, y1
-
+    # generates a series of tiles from a Frame
+    # resulting array will be the shape: (ntilesx, ntilesy, sizex, sizey)
     def load_tiles(self, frame=None, pad=False):
         if not frame is None:
             self.set_frame(frame, pad)
@@ -92,6 +119,8 @@ class Tiler:
         #       23) in this article -> https://towardsdatascience.com/advanced-numpy-master-stride-tricks-with-25-illustrated-exercises-923a9393ab20
         return np.lib.stride_tricks.as_strided(
             self.frame.img, shape=shape, strides=strides)
+    #
+    # end of load_tiles
 #
 # end of Tiler
 
