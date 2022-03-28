added parameter to max_dev to set end of line location, fixed antibody masking

classes/sana_antibody_processor.py
@@ -55,8 +55,6 @@ class Processor:
 
         # finally, mask the frame by the main ROI
         self.frame.mask(self.main_mask)
-        self.dab.mask(self.main_mask)
-        self.hem.mask(self.main_mask)        
     #
     # end of gen_masks
     
@@ -67,12 +65,11 @@ class Processor:
     # NOTE: the frame and rois must be in the same coord. system
     # TODO: any way to check the above??, could check if theres no overlap between frame nad mask
     #       IMPORTANT! not actually AO=0, if the frame is black you would still get AO=0
-    def run_ao(self, orig_frame):
+    def run_ao(self, frame):
 
-        # create a copy to not disturb the original data
-        # TODO: need to check that it is thresholded! set a flag in frame.threshold()
-        frame = orig_frame.copy()
-        
+        # apply the mask
+        frame.mask(self.main_mask)
+
         # get the total area of the roi
         area = np.sum(self.main_mask.img / 255)
 
@@ -236,7 +233,7 @@ class MBPProcessor(HDABProcessor):
         #       value is calculated from that
         # TODO: check the math!
         # TODO: this doesn't work
-        self.manual_dab_threshold = 35
+        self.manual_dab_threshold = 94
 
         # generate the manually curated AO results
         self.run_manual_ao(odir, params)
@@ -268,7 +265,7 @@ class MBPProcessor(HDABProcessor):
         # for vertical
         sigma = (10,100)
         self.vert_sta = STA(sigma)
-        self.vert_sta.run(self.dab_norm, debug=True)
+        self.vert_sta.run(self.dab_norm)
 
         # get the distance from 90, then inverse it
         # NOTE: this maps 0 and 180 -> 0, 90 -> 90
@@ -289,7 +286,7 @@ class MBPProcessor(HDABProcessor):
 
         # get the histogram and threshold
         self.vert_hist = self.vert_prob.histogram()
-        self.vert_threshold = max_dev(self.vert_hist)
+        self.vert_threshold = max_dev(self.vert_hist, mx=150)
 
         # threshold the prob. map
         self.vert_thresh = self.vert_prob.copy()
@@ -323,13 +320,13 @@ class MBPProcessor(HDABProcessor):
 
         sigma = (100,10)
         self.horz_sta = STA(sigma)
-        self.horz_sta.run(self.dab_norm, debug=True)
-        
+        self.horz_sta.run(self.dab_norm)
+
         # get the distance from 90
         # NOTE: this maps 0 and 180 -> 90, 90 -> 0
         self.horz_sta.ang = (90 - self.horz_sta.ang)
         self.horz_sta.ang /= 90
-        
+
         # create the probability map
         # NOTE: we are essentially scaling the DAB prob. by the coh and ang
         # TODO: this part is redundant with vert_ao
@@ -345,7 +342,7 @@ class MBPProcessor(HDABProcessor):
 
         # get the histogram and threshold
         self.horz_hist = self.horz_prob.histogram()
-        self.horz_threshold = max_dev(self.horz_hist)
+        self.horz_threshold = max_dev(self.horz_hist, mx=150)
 
         # threshold the prob. map
         self.horz_thresh = self.horz_prob.copy()

classes/sana_thresholds.py
@@ -127,14 +127,14 @@ def kittler(hist, mi=0, mx=255):
 # 2) draws a line from peak to end of histogram
 # 3) finds point on histogram which it's perp. line
 #     is longest, i.e. point is furthest from the line
-def max_dev(hist, scale=1.0, debug=False):
+def max_dev(hist, scale=1.0, debug=False, mx=255):
     hist_norm = hist.ravel()/hist.sum()
     hist_norm[0] = 0
     hist_norm /= np.max(hist_norm)
     hist_norm *= 255
 
     # get the peak and end of histogram
-    x0, x1 = np.argmax(hist_norm), 255
+    x0, x1 = np.argmax(hist_norm[:mx]), mx
     y0, y1 = hist_norm[x0], hist_norm[x1]
 
     # move the initial position by the ratio of the peak
@@ -153,6 +153,7 @@ def max_dev(hist, scale=1.0, debug=False):
     
     mx_dist = 0
     thresh = 0
+    pj = None    
     for xi in np.arange(x0, x1):
         yi = hist_norm[xi]
 
@@ -173,7 +174,8 @@ def max_dev(hist, scale=1.0, debug=False):
             pj = (xj, yj)
 
     if debug:
-        ax.plot([thresh, pj[0]], [hist_norm[thresh], pj[1]], color='black')
+        if pj:
+            ax.plot([thresh, pj[0]], [hist_norm[thresh], pj[1]], color='black')
         plt.show()
         
     return thresh