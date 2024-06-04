73eb2555b8b246f0070fe031c555d315a71deba1
Noah Capp <noahmcapp@gmail.com>
2024-06-04 12:43:14 -0400

    deleted old refactored code

--- classes/wildcat/util.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:10582fe2f1fbb6a39af7c61646c371beede0e875292ce19fd7ca464de21307ec
-size 6538
--- classes/wildcat/voc.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0d642068fbaeed706115004de26bbcce0fda957f4e26509784ef47b7f79b6ea8
-size 8840
